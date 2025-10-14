bff53d20f3048fafc04fcb092bcc6855882ecd91
FTC-Team-11412 <ftc11412@gmail.com>
2025-10-13 19:17:03 -0500

    Testing CommonRobot

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoAnonymous25_26/CommonRobot.java
@@ -0,0 +1,99 @@
+package org.firstinspires.ftc.teamcode.AutoAnonymous25_26;
+
+import static org.firstinspires.ftc.robotcore.external.BlocksOpModeCompanion.hardwareMap;
+
+import com.qualcomm.hardware.rev.RevHubOrientationOnRobot;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.IMU;
+
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+
+public class CommonRobot {
+    public static CommonRobot INSTANCE;
+    DcMotor [] DriveMotors = new DcMotor[4];
+    IMU imu;
+    int FL=0, FR=1, BL=2, BR=3;
+
+    public static CommonRobot getCommonRobot() {
+
+        if (INSTANCE == null) {
+            INSTANCE = new CommonRobot();
+        }
+     return INSTANCE;
+    }
+    public CommonRobot() {
+
+        DriveMotors[FL] = hardwareMap.get(DcMotor.class, "driveFrontLeft");
+        DriveMotors[FR] = hardwareMap.get(DcMotor.class, "driveFrontRight");
+        DriveMotors[BL] = hardwareMap.get(DcMotor.class, "driveBackLeft");
+        DriveMotors[BR] = hardwareMap.get(DcMotor.class, "driveBackRight");
+
+        // We set the left motors in reverse which is needed for drive trains where the left
+        // motors are opposite to the right ones.
+        DriveMotors[FL].setDirection(DcMotor.Direction.REVERSE);
+        DriveMotors[BL].setDirection(DcMotor.Direction.REVERSE);
+
+        // This uses RUN_USING_ENCODER to be more accurate.   If you don't have the encoder
+        // wires, you should remove these
+        for(int i = 0; i <4; i++){
+            DriveMotors[i].setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        }
+
+        imu = hardwareMap.get(IMU.class, "imu");
+        // This needs to be changed to match the orientation on your robot
+        RevHubOrientationOnRobot.LogoFacingDirection logoDirection =
+                RevHubOrientationOnRobot.LogoFacingDirection.FORWARD;
+        RevHubOrientationOnRobot.UsbFacingDirection usbDirection =
+                RevHubOrientationOnRobot.UsbFacingDirection.UP;
+
+        RevHubOrientationOnRobot orientationOnRobot = new
+                RevHubOrientationOnRobot(logoDirection, usbDirection);
+        imu.initialize(new IMU.Parameters(orientationOnRobot));
+    }
+    public void driveFieldRelative(double forward, double right, double rotate) {
+        // First, convert direction being asked to drive to polar coordinates
+        double theta = Math.atan2(forward, right);
+        double r = Math.hypot(right, forward);
+
+        // Second, rotate angle by the angle the robot is pointing
+        theta = AngleUnit.normalizeRadians(theta -
+                imu.getRobotYawPitchRollAngles().getYaw(AngleUnit.RADIANS));
+
+        // Third, convert back to cartesian
+        double newForward = r * Math.sin(theta);
+        double newRight = r * Math.cos(theta);
+
+        // Finally, call the drive method with robot relative forward and right amounts
+        drive(newForward, newRight, rotate);
+    }
+
+    // Thanks to FTC16072 for sharing this code!!
+    public void drive(double forward, double right, double rotate) {
+        // This calculates the power needed for each wheel based on the amount of forward,
+        // strafe right, and rotate
+        double [] motorPowers = {
+                forward + right + rotate,//FL
+                forward - right - rotate,//FR
+                forward + right - rotate,//BR
+                forward - right + rotate//BL
+        };
+
+        double maxPower = 1.0;
+        double maxSpeed = 1.0;  // make this slower for outreaches
+
+        // This is needed to make sure we don't pass > 1.0 to any wheel
+        // It allows us to keep all of the motors in proportion to what they should
+        // be and not get clipped
+        for(int i = 0; i<4; i++){
+            maxPower = Math.max(maxPower, Math.abs(motorPowers[i]));
+        }
+
+        // We multiply by maxSpeed so that it can be set lower for outreaches
+        // When a young child is driving the robot, we may not want to allow full
+        // speed.
+        for(int i = 0; i<4; i++){
+            DriveMotors[i].setPower(maxSpeed*(motorPowers[i]/maxPower));
+        }
+    }
+
+}
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoAnonymous25_26/SampleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoAnonymous25_26/SampleOp.java
@@ -1,4 +1,56 @@
 package org.firstinspires.ftc.teamcode.AutoAnonymous25_26;
 
-public class SampleOp {
+import com.qualcomm.hardware.rev.RevHubOrientationOnRobot;
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.IMU;
+
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+
+
+@TeleOp(name = "Robot: Field Relative Mecanum Drive", group = "Robot")
+public class SampleOp extends OpMode {
+
+    CommonRobot comBot;
+    // This declares the four motors needed
+    DcMotor frontLeftDrive;
+    DcMotor frontRightDrive;
+    DcMotor backLeftDrive;
+    DcMotor backRightDrive;
+    DcMotor [] DriveMotors = new DcMotor[4];
+    int FL=0, FR=1, BL=2, BR=3;
+
+    // This declares the IMU needed to get the current direction the robot is facing
+    IMU imu;
+
+    @Override
+    public void init() {
+
+        comBot = CommonRobot.getCommonRobot();
+    }
+
+    @Override
+    public void loop() {
+        telemetry.addLine("Press A to reset Yaw");
+//        telemetry.addLine("Hold left bumper to drive in robot relative");
+        telemetry.addLine("The left joystick sets the robot direction");
+        telemetry.addLine("Moving the right joystick left and right turns the robot");
+
+        // If you press the A button, then you reset the Yaw to be zero from the way
+        // the robot is currently pointing
+        if (gamepad1.a) {
+            imu.resetYaw();
+        }
+        // If you press the left bumper, you get a drive from the point of view of the robot
+        // (much like driving an RC vehicle)
+//        if (gamepad1.left_bumper) {
+//            drive(-gamepad1.left_stick_y, gamepad1.left_stick_x, gamepad1.right_stick_x);
+//        } else {
+//            driveFieldRelative(-gamepad1.left_stick_y, gamepad1.left_stick_x, gamepad1.right_stick_x);
+//        }
+        comBot.driveFieldRelative(-gamepad1.left_stick_y, gamepad1.right_stick_x, gamepad1.left_stick_x);
+    }
+
 }
