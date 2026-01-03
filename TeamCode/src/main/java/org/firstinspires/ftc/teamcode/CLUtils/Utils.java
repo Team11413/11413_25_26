0c7b480376de2b6c361bf6216efb06d2ac1a5cef
Matt Keran <mattkeran@gmail.com>
2026-01-03 12:01:19 -0600

    added util functions

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/CLUtils/Utils.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/CLUtils/Utils.java
@@ -6,6 +6,34 @@ import org.firstinspires.ftc.robotcore.external.navigation.Pose2D;
 
 public class Utils {
 
+    public static double invScaledLerp(double target, double[] scale){
+        double p = scale.length-1;
+        for (int i = (int) p; i >0; i--) {
+            p--;
+            if(target>i){
+                p+=(target-scale[i])/(scale[i+1]-scale[i]);
+                break;
+            }
+        }
+        return p;
+    }
+
+    /*
+    This accepts a target greater than 1,
+    and will return the exact scale value if the target is within the tolerance.
+     */
+    public static double scaledLerp(double t, double[] scale, double tolerance){
+        int idx=(int)t;
+        double dif= t-idx;
+        if(dif<tolerance){
+            return scale[idx];
+        }
+        if(dif>1-tolerance){
+            return scale[idx+1];
+        }
+        return scale[idx]+(scale[idx+1]-scale[idx])*dif;
+    }
+
     public static Pose2D PoseInRad(double x, double y, double r){
         return new Pose2D(DistanceUnit.INCH, x, y, AngleUnit.RADIANS, r);
     }
